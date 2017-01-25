869283652c6f32aa7233243d336f5a018bf17912
Brandon <sourbrandon@gmail.com>
2017-01-25 14:51:00 +0800

    added minion class and lanes

---
+++ core/src/com/progclub/pixelparty/Minion.java
@@ -0,0 +1,11 @@
+package com.progclub.pixelparty;
+
+/**
+ * Created by Brandon on 1/25/17.
+ */
+
+public class Minion extends GameObject {
+    protected Minion(int width, int height) {
+        super(width, height);
+    }
+}
--- core/src/com/progclub/pixelparty/PixelPartyGame.java
+++ core/src/com/progclub/pixelparty/PixelPartyGame.java
@@ -5,29 +5,60 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 
 public class PixelPartyGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	private Rectangle field = new Rectangle();
+	private ShapeRenderer shapeRenderer;
+	private float fieldTop, fieldBot, fieldLeft, fieldRight;
+	private Minion minion;
+	private float totalWidth, totalHeight;
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		totalWidth = Gdx.graphics.getWidth();
+		totalHeight = Gdx.graphics.getHeight();
+		field.set(0,0,totalWidth, totalHeight);
+		fieldLeft = field.x;
+		fieldRight = field.x+field.width;
+		fieldBot = field.y;
+		fieldTop = field.y+field.height;
+		shapeRenderer = new ShapeRenderer();
+
+		minion = new Minion(50,50);
+		minion.move(field.x + (field.width - minion.getWidth()) / 2, field.y + (field.height - minion.getHeight()) / 2);
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		draw();
 	}
-	
+
+	public void draw(){
+		Gdx.gl.glClearColor(0f,0f,0f,1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.rect(minion.getX(),minion.getY(),minion.getWidth(), minion.getHeight());
+		drawLanes(4);
+		shapeRenderer.end();
+	}
+
+	public void drawLanes(int num){
+		int width = (int)(totalWidth/100);
+		int interval = (int)(totalWidth/num);
+		for (int i = interval; i <= totalWidth-interval; i+=interval){
+			shapeRenderer.rect(i-width,0,width, totalHeight);
+		}
+
+	}
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
