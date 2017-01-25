c29438640dd3613e525fd0c49398decf5ece1f1d
Brandon <sourbrandon@gmail.com>
2017-01-25 14:20:20 +0800

    Initial commit

---
+++ android/src/com/progclub/pixelparty/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.progclub.pixelparty;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.progclub.pixelparty.PixelPartyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PixelPartyGame(), config);
+	}
+}
