66451c8e94b072118118d86c8e39096e32d39cb4
Brandon <sourbrandon@gmail.com>
2017-01-28 12:39:41 +0800

    working start activity

--- android/src/com/saspxprogclub/pixelparty/StartActivity.java
+++ android/src/com/saspxprogclub/pixelparty/StartActivity.java
@@ -1,13 +1,26 @@
 package com.saspxprogclub.pixelparty;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Button button = (Button)findViewById(R.id.buttonSingle);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), AndroidLauncher.class);
+                view.getContext().startActivity(intent);
+            }
+        });
     }
 }
