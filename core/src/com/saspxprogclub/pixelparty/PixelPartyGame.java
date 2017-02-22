b55b1091bd96ca274763b24a033ef2dd70bdc886
Ryan Chang <rzinshanghai@gmail.com>
2017-02-22 13:32:09 +0800

    test for ryan

--- core/src/com/saspxprogclub/pixelparty/PixelPartyGame.java
+++ core/src/com/saspxprogclub/pixelparty/PixelPartyGame.java
@@ -15,6 +15,8 @@ import com.badlogic.gdx.math.Vector2;
 import java.util.ArrayList;
 import java.util.List;
 
+//ryan was here
+
 public class PixelPartyGame implements ApplicationListener, InputProcessor {
 
 	public static int verticalBuffer;
