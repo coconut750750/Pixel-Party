60a851f884e8612174e7bc84c5015c8e2ae443ea
Brandon <sourbrandon@gmail.com>
2017-03-02 17:50:46 +0800

    changed gameobject classes

--- core/src/com/saspxprogclub/pixelparty/Mana.java
+++ core/src/com/saspxprogclub/pixelparty/Mana.java
@@ -2,6 +2,7 @@ package com.saspxprogclub.pixelparty;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 
 import static com.saspxprogclub.pixelparty.PixelPartyGame.field;
 
@@ -21,17 +22,24 @@ public class Mana extends GameObject {
     private int margin;
     private int widthEach;
 
+    private int width;
+    private int height;
+
     /**
      * constructor
      * @param width  of the object (minions, maybe towers later)
      * @param height of the object
      */
     Mana(int width, int height, int margin, int initial) {
-        super(width-2*margin, height-margin);
+        bounds = new Rectangle();
+        ((Rectangle)bounds).setWidth(width-2*margin);
+        ((Rectangle)bounds).setHeight(height-margin);
         this.count = initial;
         this.margin = margin;
         this.widthEach = (int)(Math.floor(getWidth()/10f));
         this.delay = DELAY;
+        this.width = width;
+        this.height = height;
     }
 
     /**
@@ -50,8 +58,49 @@ public class Mana extends GameObject {
     }
 
     @Override
+    void setBounds(float x, float y) {
+        ((Rectangle)bounds).set(x, y, width ,height);
+    }
+
+    @Override
+    void updateBounds() {
+        ((Rectangle)bounds).setX(getX());
+        ((Rectangle)bounds).setY(getY());
+    }
+
+    @Override
+    float getHeight() {
+        return ((Rectangle)bounds).getHeight();
+    }
+
+    @Override
+    float getWidth() {
+        return ((Rectangle)bounds).getWidth();
+    }
+
+    @Override
     float getX() {
         return (field.width-widthEach*10)/2;
+    }
+
+    @Override
+    float bottom() {
+        return ((Rectangle)bounds).y;
+    }
+
+    @Override
+    public float left() {
+        return ((Rectangle)bounds).x;
+    }
+
+    @Override
+    public float right() {
+        return ((Rectangle)bounds).x+((Rectangle)bounds).getWidth();
+    }
+
+    @Override
+    float top() {
+        return ((Rectangle)bounds).y+((Rectangle)bounds).getHeight();
     }
 
     /**
