931b0ca7c85f3ac5258c283c9737d25e9a027f5a
Brandon <sourbrandon@gmail.com>
2017-03-05 15:03:22 +0800

    changed speed

--- core/src/com/saspxprogclub/pixelparty/Minions/Wizard.java
+++ core/src/com/saspxprogclub/pixelparty/Minions/Wizard.java
@@ -13,7 +13,7 @@ public class Wizard extends Minion {
 
     private static int width = (int)(field.height/30f); //inverse
     private static int height = (int)(field.height/30f); //inverse
-    private static int vely = (int)(field.height/10f); //inverse
+    private static int vely = (int)(field.height/20f); //inverse
     private static int range = (int)(field.height/30f); //inverse
     private static int cost = 2;
     private static int damage = 250;
